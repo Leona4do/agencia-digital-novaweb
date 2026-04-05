d6e201d4ac315980fbbdf4afc504f2cda7a22d41
Keiner <118keiner@gmail.com>
2026-04-05 17:22:54 -0400

    Título actualizado index.php
    Se modifica título de encabezado.

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>NovaWeb Studio | Agencia Digital</title>
+    <title>NovaWeb Studio | Tu Agencia Digital</title>
     <!-- Fuente Poppins desde Google Fonts -->
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700&display=swap" rel="stylesheet">
     <!-- Iconos Font Awesome -->
